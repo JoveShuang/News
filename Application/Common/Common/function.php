8d78644f1d5487e3e2fd3228e1bca03bebd023cb
iexn <iexn@qq.com>
2018-03-16 16:38:12 +0800

    0.04

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -41,3 +41,29 @@ function getActive($navc){
     }
     return '';
 }
+function showKind($status,$data){
+    header('Content-type:application/json;charset=UTF-8');
+    if($status==0){
+        exit(json_encode(array('error'=>0,'url'=>$data)));
+    }
+    exit(json_encode(array('error'=>1,'message'=>'上传失败')));
+}
+function getLoginUsername(){
+    return $_SESSION['adminUser']['username'] ? $_SESSION['adminUser']['username']:'';
+}
+function getCatName($navs,$id){
+    foreach($navs as $nav){
+        $navList[$nav['menu_id']] = $nav['name'];
+    }
+    return isset($navList[$id]) ? $navList[$id] : '';
+}
+function getCopyFromById($id){
+    $copyFrom = C("COPY_FROM");
+    return $copyFrom[$id] ? $copyFrom[$id] : '';
+}
+function isThumb($thumb){
+    if($thumb){
+        return '<span style="color:"red">有</span>';
+    }
+    return '无';
+}
